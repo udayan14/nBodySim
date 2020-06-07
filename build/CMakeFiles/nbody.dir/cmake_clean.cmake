a4ced6d674bbc917c0d0e0ac4a988da3f0d611f2
udayan14 <14udayanjoshi@gmail.com>
2020-06-06 23:22:01 -0700

    Fixed repo structure

---
+++ build/CMakeFiles/nbody.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nbody.dir/src/particles.cpp.o"
+  "CMakeFiles/nbody.dir/src/solver.cpp.o"
+  "CMakeFiles/nbody.dir/src/test.cpp.o"
+  "nbody.pdb"
+  "nbody"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/nbody.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
